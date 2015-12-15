0e33c588b40213fc2a7d3901440d51c81b117944
Stephen Brennan <smb196@case.edu>
2015-12-15 13:42:31 -0500

    Let's hope this works!

---
+++ push.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# Build GitHub Pages branch automatically.
+## STARTING STATE: Must be in the branch master, with no uncommitted changes.
+## You've run the tests on release mode, and they run successfully!
+
+# Exit script on first error:
+set -e
+
+# Get the current commit
+COMMIT=$(git rev-parse HEAD)
+
+# Generate coverage information.
+make CFG=coverage cov
+
+# Generate documentation.
+make doc
+
+# In a new directory, clone the gh-pages branch.
+cd ..
+git clone -b gh-pages "https://$GH_TOKEN@github.com/brenns10/nosj.git" gh-pages
+cd gh-pages
+
+# Update git configuration so I can push.
+if [ "$1" != "dry" ]; then
+    # Update git config.
+    git config user.name "Travis Builder"
+    git config user.email "example@example.com"
+fi
+
+# Copy the docs and coverage results.
+rm -rf doc
+cp -R ../nosj/doc/_build/html doc
+cp -R ../nosj/cov .
+
+# Add and commit changes.
+git add -A .
+git commit -m "[ci skip] Autodoc commit for $COMMIT."
+if [ "$1" != "dry" ]; then
+    git push -q origin gh-pages
+fi
