abd31ce18625c2085356c70e216855002bd8cd48
Stephen Brennan <stephen@brennan.io>
2021-09-04 00:14:15 -0700

    Adjust push.sh to run locally

--- push.sh
+++ push.sh
@@ -7,30 +7,42 @@
 set -e
 
 # Get the current commit
+REPO=brenns10/nosj
 COMMIT=$(git rev-parse HEAD)
+WORK="$(pwd)"
 
-# Generate coverage information.
-make CFG=coverage cov
+# Build, test, coverage
+rm -rf build
+meson build -Db_coverage=true
+ninja -C build test
+ninja -C build coverage-html
 
 # Generate documentation.
-make doc
+doxygen
+make -C doc html
 
 # In a new directory, clone the gh-pages branch.
-cd ..
-git clone -b gh-pages "https://$GH_TOKEN@github.com/brenns10/nosj.git" gh-pages
-cd gh-pages
+rm -rf /tmp/gh-pages
+if [ -z "$GH_TOKEN" ]; then
+	git clone -b gh-pages git@github.com:$REPO /tmp/gh-pages
+else
+	git clone -b gh-pages "https://$GH_TOKEN@github.com/$REPO" /tmp/gh-pages
+fi
+pushd /tmp/gh-pages
+rm -rf doc
+rm -rf cov
 
 # Update git configuration so I can push.
 if [ "$1" != "dry" ]; then
     # Update git config.
-    git config user.name "Travis Builder"
-    git config user.email "example@example.com"
+    git config --local user.name "Pages Builder"
+    git config --local user.email "example@example.com"
 fi
 
 # Copy the docs and coverage results.
 rm -rf doc
-cp -R ../nosj/doc/_build/html doc
-cp -R ../nosj/cov .
+cp -R $WORK/doc/_build/html doc
+cp -R $WORK/build/meson-logs/coveragereport ./cov
 
 # Add and commit changes.
 git add -A .
@@ -38,3 +50,5 @@ git commit -m "[ci skip] Autodoc commit for $COMMIT."
 if [ "$1" != "dry" ]; then
     git push -q origin gh-pages
 fi
+popd
+rm -rf /tmp/gh-pages
