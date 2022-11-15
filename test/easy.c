17c675edb16413d96ad36e89f3454c6329d36dcf
Stephen Brennan <stephen@brennan.io>
2022-11-14 22:03:04 -0800

    Add silly tests

---
+++ test/easy.c
@@ -0,0 +1,63 @@
+/*
+ * test/easy.c: Test the "json_easy" API
+ */
+
+#include <unity.h>
+
+#include "nosj.h"
+#include "twitapi.h"
+
+void setUp(void)
+{
+	// set stuff up here
+}
+
+void tearDown(void)
+{
+	// clean stuff up here
+}
+
+static void test_basic_access(void)
+{
+	struct json_easy *easy = json_easy_new(twitapi_json);
+	int result = json_easy_parse(easy);
+	TEST_ASSERT_EQUAL(0, result);
+	size_t index = json_easy_object_get(easy, 0, "retweet_count");
+	TEST_ASSERT_NOT_EQUAL(0, index);
+	TEST_ASSERT_EQUAL(66, json_easy_number_get(easy, index));
+	json_easy_free(easy);
+}
+
+static void test_easy_string(void)
+{
+	struct json_easy *easy = json_easy_new(twitapi_json);
+	int result = json_easy_parse(easy);
+	TEST_ASSERT_EQUAL(0, result);
+	size_t index = json_easy_lookup(easy, 0, "user.name");
+	TEST_ASSERT_NOT_EQUAL(0, index);
+	char *data = json_easy_string_get(easy, index);
+	TEST_ASSERT_EQUAL_STRING("Twitter API", data);
+	free(data);
+	json_easy_free(easy);
+}
+
+static void test_parse_fail(void)
+{
+	struct json_easy *easy = json_easy_new("[1, 2");
+	int result = json_easy_parse(easy);
+	TEST_ASSERT_EQUAL(-JSONERR_MISSING_COMMA, result);
+	TEST_ASSERT_EQUAL_STRING("expected comma between elements",
+	                         json_easy_strerror(result));
+	json_easy_free(easy);
+}
+
+int main(void)
+{
+	UNITY_BEGIN();
+
+	RUN_TEST(test_basic_access);
+	RUN_TEST(test_easy_string);
+	RUN_TEST(test_parse_fail);
+
+	return UNITY_END();
+}
